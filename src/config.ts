c99e40ac47bd6e0dc7379f5cd1cbd990b9b414bd
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-01-15 09:05:29 +0100

    added config.ts

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+type Config = {
+  author: string
+  title: string
+  description: string
+  lang: string
+}
+
+export default {
+  author: 'Samuel Breznjak',
+  title: 'Minimal = Astro blog theme',
+  description: 'Minimalistic blog theme for astro',
+  lang: 'en',
+} satisfies Config
