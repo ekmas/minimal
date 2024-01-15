bb95aa54e32af5ae9aea979e77b484408f9ac52a
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-01-15 04:23:45 +0100

    added font sora

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Sora', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
