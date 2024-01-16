40cc687995fa2f20ea07b5518d125bbcba7b9095
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-01-16 04:48:26 +0100

    added content config

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content'
+
+function removeDupsAndLowerCase(array: string[]) {
+  if (!array.length) return array
+  const lowercaseItems = array.map((str) => str.toLowerCase())
+  const distinctItems = new Set(lowercaseItems)
+  return Array.from(distinctItems)
+}
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+  }),
+})
+
+export const collections = { blog }
