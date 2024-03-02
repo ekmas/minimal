a8ebbb8b33e54cdf59703e5ce4147d826c093883
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-02 05:29:33 +0100

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   site: 'https://astro-minimal.netlify.app/',
 })
